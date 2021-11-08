7cb9a92f4c1078f63cfa9fdb95cd1804d6b7b45b
zxf <1019807613@qq.com>
2021-11-08 19:26:20 +0800

    更新Department表的查找

--- vhrdemo/src/main/java/com/example/vhrdemo/service/IDepartmentService.java
+++
@@ -1,9 +0,0 @@
-package com.example.vhrdemo.service;
-
-import com.example.vhrdemo.entity.Department;
-import java.util.List;
-
-public interface IDepartmentService {
-
-    List<Department> findall();
-}
