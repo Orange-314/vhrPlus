51081606965a6c65d54e9396de4a3c3fc687cb50
zxf <1019807613@qq.com>
2021-11-08 18:30:07 +0800

    更新Department表的查找

---
+++ vhrdemo/src/main/java/com/example/vhrdemo/controller/DepartmentController.java
@@ -0,0 +1,26 @@
+package com.example.vhrdemo.controller;
+
+import com.example.vhrdemo.entity.Department;
+import com.example.vhrdemo.entity.Employee;
+import com.example.vhrdemo.service.IDepartmentService;
+import com.example.vhrdemo.service.IEmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping(value = "/department")
+public class DepartmentController {
+    @Autowired
+    private IDepartmentService iDepartmentService;
+
+    @RequestMapping(value = "/findall", method = RequestMethod.POST)
+    private List<Department> findall(){
+        return iDepartmentService.findall();
+    }
+}
---
+++ vhrdemo/src/main/java/com/example/vhrdemo/service/impl/DepartmentService.java
@@ -0,0 +1,21 @@
+package com.example.vhrdemo.service.impl;
+
+import com.example.vhrdemo.dao.IDepartmentDao;
+import com.example.vhrdemo.entity.Department;
+import com.example.vhrdemo.entity.Employee;
+import com.example.vhrdemo.service.IDepartmentService;
+import com.example.vhrdemo.service.IEmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class DepartmentService implements IDepartmentService {
+    @Autowired
+    private IDepartmentDao iDepartmentDao;
+
+    @Override
+    public List<Department> findall() {
+        return iDepartmentDao.findall();
+    }
+}
