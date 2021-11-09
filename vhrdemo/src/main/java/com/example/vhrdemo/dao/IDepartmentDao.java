87833f2ad6f29b9b3ba9e84789eb2f1259b955ae
zxf <1019807613@qq.com>
2021-11-09 18:25:28 +0800

    更新包名

--- vhrdemo/src/main/java/com/example/vhrdemo/dao/IDepartmentDao.java
+++ vhrdemo/src/main/java/com/example/vhrdemo/dao/IDepartmentDao.java
@@ -1,7 +1,6 @@
 package com.example.vhrdemo.dao;
 
 import com.example.vhrdemo.entity.Department;
-import com.example.vhrdemo.entity.Employee;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
--- vhrdemo/src/main/java/com/example/vhrdemo/service/IDepartmentService.java
+++ vhrdemo/src/main/java/com/example/vhrdemo/service/IDepartmentService.java
@@ -1,7 +1,6 @@
 package com.example.vhrdemo.service;
 
 import com.example.vhrdemo.entity.Department;
-import com.example.vhrdemo.entity.Employee;
 
 import java.util.List;
 
