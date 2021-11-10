5c762925cb7614ca8197a898c3e0197dff26f22d
Dong <1714666834@qq.com>
2021-11-10 13:02:19 +0800

    更新了安卓前端的员工显示一览和员工详细信息跳转（已解决）

--- vhr_android_demo/app/src/main/java/com/example/vhr_android_demo/MainActivity.java
+++ vhr_android_demo/app/src/main/java/com/example/vhr_android_demo/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.vhr_android_demo;
 
+import static java.lang.Thread.sleep;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -90,7 +92,15 @@ public class MainActivity extends AppCompatActivity {
                                     }
                                     if (!responseData.equals("")) {
                                         Toast.makeText(MainActivity.this, "登录成功", Toast.LENGTH_SHORT).show();//显示登录成功！
-                                        Intent loginDone = new Intent(MainActivity.this, LoginDoneActivity.class);
+                                        Intent loginDone = new Intent(MainActivity.this, EmployeeActivity.class);
+
+
+                                        try {
+                                            sleep(100);
+                                        } catch (InterruptedException e) {//等待100ms
+                                            e.printStackTrace();
+                                        }
+
                                         startActivity(loginDone);
 
                                     } else {
