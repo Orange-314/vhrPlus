f5e518fb55733fad6f57b8a90f3eb79c8b452fce
zxf <1019807613@qq.com>
2021-11-14 21:38:22 +0800

    更新salary的insert和insert1(第三版，两个insert方法传值不用传id，id自增长)

--- vhrdemo/src/main/java/com/example/vhrdemo/service/ISalaryService.java
+++ vhrdemo/src/main/java/com/example/vhrdemo/service/ISalaryService.java
@@ -9,9 +9,9 @@ public interface ISalaryService {
 
     Salary search(String name);
 
-    Salary insert1(int id,int basicSalary,int bonus,int lunchSalary,int trafficSalary,int allSalary);
+    Salary insert1(int basicSalary,int bonus,int lunchSalary,int trafficSalary,int allSalary);
 
-    Salary insert(int id,int basicSalary,int bonus,int lunchSalary,int trafficSalary,int allSalary,int pensionBase,float pensionPer,int medicalBase,float medicalPer,int accumulationFundBase,float accumulationFundPer,String name);
+    Salary insert(int basicSalary,int bonus,int lunchSalary,int trafficSalary,int allSalary,int pensionBase,float pensionPer,int medicalBase,float medicalPer,int accumulationFundBase,float accumulationFundPer,String name);
 
     Salary update(int id,int basicSalary,int bonus,int lunchSalary,int trafficSalary,int allSalary,int pensionBase,float pensionPer,int medicalBase,float medicalPer,int accumulationFundBase,float accumulationFundPer,String name);
 
